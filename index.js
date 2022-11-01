2caa360b1f922c43e2a5798f0a87f0c441f51633
Maxwell Gottesman <maxgman16@yahoo.com>
2022-10-31 23:35:50 -0400

    commit tree 7c6d9af21e45db07c970945be00da5d5f2f58cf4

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Your code here
+
+const dodger = document.getElementById('dodger');
+
+dodger.style.backgroundColor = '#a9a9a9';
+
+document.addEventListener('keydown',function(e) {
+    if(e.key === 'ArrowLeft') moveDodgerLeft();
+    else if(e.key === 'ArrowRight') moveDodgerRight();
+});
+
+function moveDodgerLeft() {
+    const x = parseInt(dodger.style.left);
+    if(x > 0 ) dodger.style.left = `${parseInt(x,10)-1}px`;
+}
+
+function moveDodgerRight() {
+    const x = parseInt(dodger.style.left);
+    if(x < 360 ) dodger.style.left = `${parseInt(x,10)+1}px`;
+}
